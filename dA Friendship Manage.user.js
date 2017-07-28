fc87d1fb899956b2d250fcb1f7d3d98498d81bf8
Daniel Vargas <dvargas2013@gmail.com>
2017-07-28 04:05:08 -0400

    wrote a thing for dA

---
+++ dA Friendship Manage.user.js
@@ -0,0 +1,27 @@
+// ==UserScript==
+// @name         dA Friendship Manage
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        http://www.deviantart.com/watching*
+// @grant        none
+// ==/UserScript==
+
+var hundred;
+
+setTimeout(function(){
+    'use strict';
+    document.body.onkeydown = function(e) {
+        if (e.which !== 84) return;
+        hundred = document.querySelectorAll('tbody.grp tr.friend');
+        for (var i=0; i<hundred.length; i++) {
+            hundred[i].children[4].children[0].classList.remove('checked');
+            hundred[i].children[5].children[0].classList.remove('checked');
+            hundred[i].children[6].children[0].classList.remove('checked');
+            hundred[i].children[7].children[0].classList.remove('checked');
+            hundred[i].children[8].children[0].classList.remove('checked');
+            hundred[i].children[8].children[0].click();
+        }
+    }
+}, 2000);
